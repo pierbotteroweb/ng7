fa3a259590e8e69ec6a2bd86a30008f7e588bdf4
pierbotteroweb <pierbotteroweb@gmail.com>
2019-08-04 14:18:30 -0300

    Components, interpolação, routing, event binding, class e style binding

---
+++ ng7/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  h1Style:boolean=false
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  firstClick(){
+    console.log("Clicked")
+    this.h1Style=!this.h1Style
+  }
+
+}
